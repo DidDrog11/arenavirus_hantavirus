c81b36365d94212bcc286c984b8aed8c37f7b68d
David Simons <52014150+DidDrog11@users.noreply.github.com>
2024-02-01 14:51:14 -0800

    begin cleaning pathogen data

---
+++ R/01_clean_data.R
@@ -0,0 +1,52 @@
+source(here::here("R", "00_load_data.R"))
+
+virus_names <- tibble(virus = sort(unique(combined_data$pathogen$scientificName))) %>%
+  mutate(virus_clean = case_when(str_detect(tolower(virus), "andes") ~ "Andes orthohantavirus",
+                                 str_detect(tolower(virus), "araraqua") ~ "Araraquara orthohantavirus",
+                                 str_detect(tolower(virus), "asama") ~ "Asama orthohantavirus",
+                                 str_detect(tolower(virus), "bayou|bayoui") ~ "Bayou orthohantavirus",
+                                 str_detect(tolower(virus), "chapare") ~ "Chapare nannarenavirus",
+                                 str_detect(tolower(virus), "dobrova|dobrava|belgrade|dobravaense") ~ "Dobrava-Belgrade orthohantavirus",
+                                 str_detect(tolower(virus), "hantaan") ~ "Hantaan orthohantavirus",
+                                 str_detect(tolower(virus), "junin") ~ "Junin mammarenavirus",
+                                 str_detect(tolower(virus), "juquitiba") & !str_detect(tolower(virus), "jabora") ~ "Juquitiba orthohantavirus",
+                                 str_detect(tolower(virus), "lassa") ~ "Lassa mammarenavirus",
+                                 str_detect(tolower(virus), "lcmv|lymphocytic") ~ "Lymphocytic choriomeningitis mammarenavirus",
+                                 str_detect(tolower(virus), "muleshoe") ~ "Muleshoe orthohantavirus",
+                                 str_detect(tolower(virus), "puumala") & !str_detect(tolower(virus), "saaremaa") ~ "Puumala orthohantavirus",
+                                 str_detect(tolower(virus), "rusne") ~ "Rusne orthohantavirus",
+                                 str_detect(tolower(virus), "seewis") ~ "Seewis orthohantavirus",
+                                 str_detect(tolower(virus), "seoul") ~ "Seoul orthohantavirus",
+                                 str_detect(tolower(virus), "sin nombre") ~ "Sin Nombre orthohantavirus",
+                                 str_detect(tolower(virus), "thailand") ~ "Thailand orthohantavirus",
+                                 str_detect(tolower(virus), "tula") ~ "Tula orthohantavirus",
+                                 str_detect(tolower(virus), "wenzhou") ~ "Wenzhou mammarenavirus",
+                                 TRUE ~ virus))
+
+assay_clean <- tibble(assay = sort(unique(combined_data$pathogen$identificationRemarks))) %>%
+  mutate(assay_clean = case_when(str_detect(tolower(assay), "elisa|antibody|antigen|ifa|immuno|serology") ~ "Serology",
+                                 str_detect(tolower(assay), "pcr") ~ "PCR"))
+
+# Clean Pathogen sheet ----------------------------------------------------
+
+combined_data$pathogen <- combined_data$pathogen %>%
+  mutate(host_genus = str_to_sentence(host_genus),
+         associatedTaxa = str_to_sentence(associatedTaxa),
+         decimalLatitude = round(decimalLatitude, 2),
+         decimalLongitude = round(decimalLongitude, 2),
+         family = case_when(str_detect(tolower(family), "hanta") ~ "Hantaviridae",
+                            str_detect(tolower(family), "arena") ~ "Mammarenaviridae")) %>%
+  left_join(virus_names, by = c("scientificName" = "virus")) %>%
+  select(-scientificName, scientificName = virus_clean) %>%
+  left_join(assay_clean, by = c("identificationRemarks" = "assay")) %>%
+  select(-identificationRemarks, identificationRemarks = assay_clean) %>%
+  mutate(occurrenceRemarks = as.numeric(occurrenceRemarks),
+         number_negative = as.numeric(number_negative),
+         organismQuantity = as.numeric(organismQuantity),
+         number_inconclusive = as.numeric(number_inconclusive)) %>%
+  mutate(number_negative = replace_na(number_negative, 0),
+         organismQuantity = replace_na(organismQuantity, 0),
+         number_inconclusive = replace_na(number_inconclusive, 0)) %>%
+  select(any_of(names(combined_data$pathogen)))
+
+write_rds(combined_data, here("data", "clean_data", paste0(Sys.Date(), "_data.rds")))
