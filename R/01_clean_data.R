047687e2cd98c664f5285a9d80bc92743fbf5ab4
David Simons <52014150+DidDrog11@users.noreply.github.com>
2024-03-22 10:39:45 -0700

    link sequences

--- R/01_clean_data.R
+++ R/01_clean_data.R
@@ -57,4 +57,49 @@ combined_data$pathogen <- combined_data$pathogen %>%
          number_inconclusive = replace_na(number_inconclusive, 0)) %>%
   select(any_of(names(combined_data$pathogen)))
 
+
+# Clean sequence_data sheet -----------------------------------------------
+virus_names <- tibble(virus = sort(unique(combined_data$sequence_data$scientificName))) %>%
+  mutate(virus_clean = case_when(str_detect(tolower(virus), "andes") ~ "Andes orthohantavirus",
+                                 str_detect(tolower(virus), "araraqua") ~ "Araraquara orthohantavirus",
+                                 str_detect(tolower(virus), "asama") ~ "Asama orthohantavirus",
+                                 str_detect(tolower(virus), "bayou|bayoui") ~ "Bayou orthohantavirus",
+                                 str_detect(tolower(virus), "bitu") ~ "Bitu mammarenavirus",
+                                 str_detect(tolower(virus), "chapare") ~ "Chapare mannarenavirus",
+                                 str_detect(tolower(virus), "dobrova|dobrava|belgrade|dobravaense") ~ "Dobrava-Belgrade orthohantavirus",
+                                 str_detect(tolower(virus), "hantaan|hantann") ~ "Hantaan orthohantavirus",
+                                 str_detect(tolower(virus), "junin") ~ "Junin mammarenavirus",
+                                 str_detect(tolower(virus), "juquitiba") & !str_detect(tolower(virus), "jabora") ~ "Juquitiba orthohantavirus",
+                                 str_detect(tolower(virus), "kitale") ~ "Kitale mammarenavirus",
+                                 str_detect(tolower(virus), "kwanza") ~ "Kwanza mammarenavirus",
+                                 str_detect(tolower(virus), "lassa") ~ "Lassa mammarenavirus",
+                                 str_detect(tolower(virus), "lcmv|lymphocytic") ~ "Lymphocytic choriomeningitis mammarenavirus",
+                                 str_detect(tolower(virus), "limestone") ~ "Limestone Canyon orthohantavirus",
+                                 str_detect(tolower(virus), "muleshoe") ~ "Muleshoe orthohantavirus",
+                                 str_detect(tolower(virus), "puumala") & !str_detect(tolower(virus), "saaremaa") ~ "Puumala orthohantavirus",
+                                 str_detect(tolower(virus), "rusne") ~ "Rusne orthohantavirus",
+                                 str_detect(tolower(virus), "seewis") ~ "Seewis orthohantavirus",
+                                 str_detect(tolower(virus), "seoul") ~ "Seoul orthohantavirus",
+                                 str_detect(tolower(virus), "sin nombre") ~ "Sin Nombre orthohantavirus",
+                                 str_detect(tolower(virus), "thailand") ~ "Thailand orthohantavirus",
+                                 str_detect(tolower(virus), "tula") ~ "Tula orthohantavirus",
+                                 str_detect(tolower(virus), "wenzhou") ~ "Wenzhou mammarenavirus",
+                                 TRUE ~ virus))
+
+
+combined_data$sequence_data <- combined_data$sequence_data %>%
+  mutate(study_id = as.numeric(study_id)) %>%
+  left_join(virus_names, by = c("scientificName" = "virus")) %>%
+  left_join(combined_data$host %>%
+              select(rodent_record_id, eventDate, coordinate_resolution, decimalLatitude, decimalLongitude, individualCount),
+            by = c("associated_rodent_record_id" = "rodent_record_id")) %>%
+  left_join(combined_data$pathogen %>%
+              select(pathogen_record_id, coordinate_resolution, decimalLatitude, decimalLongitude),
+            by = c("associated_pathogen_record_id" = "pathogen_record_id")) %>%
+  mutate(coordinate_resolution = coalesce(coordinate_resolution.y, coordinate_resolution.x),
+         decimalLatitude = coalesce(decimalLatitude.y, decimalLatitude.x),
+         decimalLongitude = coalesce(decimalLongitude.y, decimalLongitude.x)) %>%
+  select(sequence_record_id, associated_rodent_record_id, associated_pathogen_record_id, eventDate, study_id, host_genus, associatedTaxa,
+         sequenceType, virus_clean, coordinate_resolution, decimalLatitude, decimalLongitude, accession_number, method, note, date_sampled, sample_location)
+
 write_rds(combined_data, here("data", "clean_data", paste0(Sys.Date(), "_data.rds")))
